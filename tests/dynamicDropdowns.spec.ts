3fee661e8715a456296318ee633c188418fa9003
LikithMD <likith8267@gmail.com>
2026-04-09 10:17:20 +0530

    Added Hidden Dropdown tests

--- tests/dynamicDropdowns.spec.ts
+++ tests/dynamicDropdowns.spec.ts
@@ -1,9 +1,27 @@
 import {test, expect, Locator} from '@playwright/test';
 
-test('Form actions test', async ({page}) => {
-    await page.goto('https://testautomationpractice.blogspot.com/');
+test('Dynamic Dropdown test', async ({page}) => {
+    await page.goto('https://www.amazon.com/');
 
-    expect(page.url()).toBe('https://testautomationpractice.blogspot.com/');
+    expect(page.url()).toBe('https://www.amazon.com/');
 
-    // To be continued tomorrow
+    await page.getByLabel('Search Amazon').click();
+    await page.getByPlaceholder('Search Amazon').fill('smart');
+
+    const allOptions: Locator = page.getByRole('row');
+
+    await expect(allOptions).toHaveCount(10);
+
+    const optionsText: string[] = (await allOptions.allTextContents()).map(option => option.trim());
+
+    console.log(optionsText);
+
+    for(let i = 0; i < optionsText.length; i++) {
+        if(optionsText[i] === 'smart watches for men') {
+            await allOptions.nth(i).click();
+            break;
+        }
+    }
+
+    await expect(page.getByRole('heading', {name: 'Results', exact: true})).toBeVisible();
 });
